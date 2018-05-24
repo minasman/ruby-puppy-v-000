cf81bd7e4158e89f5599f4086d2561e59c1f922d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-24 21:14:27 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  attr_accessor = :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all.each {|dog| puts dog}
+  end
+  
+  
+end
